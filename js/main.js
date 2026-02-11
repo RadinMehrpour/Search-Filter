413e7b73ad5f362961da6efcda79680583e1a2b3
00Radin <radin1385@yahoo.com>
2026-02-11 13:43:45 +0330

    updated

--- js/main.js
+++ js/main.js
@@ -2,25 +2,29 @@ let myInput = document.querySelector('.wrapper input');
 let allListItems = document.querySelectorAll('.wrapper ul li');
 let wrapperList = document.querySelector('.wrapper ul');
 
-      for (let i = 0; i < allListItems.length; i++) {
-        allListItems[i].addEventListener('click', function () {
-          myInput.value = this.innerHTML.trim();
-          wrapperList.style.display = 'none';
-        });
-      }
+for (let i = 0; i < allListItems.length; i++) {
+  allListItems[i].addEventListener('click', function () {
+    myInput.value = this.innerText.trim();
+    wrapperList.style.display = 'none';
+  });
+}
 
-      myInput.addEventListener('click', function () {
-        wrapperList.style.display = 'block';
-      });
+myInput.addEventListener('click', function () {
+  wrapperList.style.display = 'block';
+});
 
-      myInput.addEventListener('keyup', function () {
-        for (let i = 0; i < allListItems.length; i++) {
-          if (
-            allListItems[i].innerHTML.trim().startsWith(myInput.value.trim())
-          ) {
-            allListItems[i].style.display = 'block';
-          } else {
-            allListItems[i].style.display = 'none';
-          }
-        }
-      });
+myInput.addEventListener('keyup', function () {
+
+  let inputValue = myInput.value.trim().toLowerCase();
+
+  for (let i = 0; i < allListItems.length; i++) {
+
+    let itemText = allListItems[i].innerText.trim().toLowerCase();
+
+    if (itemText.includes(inputValue)) {
+      allListItems[i].style.display = 'block';
+    } else {
+      allListItems[i].style.display = 'none';
+    }
+  }
+});
